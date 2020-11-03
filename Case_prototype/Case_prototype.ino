cfaf33b5c91a5329718e3c9041d6fdee2c07f28f
Rajmond1999 <72790807+Rajmond1999@users.noreply.github.com>
2020-11-03 10:24:30 +0200

    1 +case

---
+++ Case_prototype/Case_prototype.ino
@@ -0,0 +1,29 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int code=1;
+  //  reading code
+  if (Serial.available() > 0) {
+    code = Serial.read();
+    code-='0';
+    }
+   Serial.println(code);
+  switch(code){
+  case 1: Serial.print("1\n"); break;;
+
+  case 2: Serial.print("2\n"); break;
+
+  case 3: Serial.print("3\n"); break;
+
+  case 4: Serial.print("4\n"); break;
+  
+  case 5: Serial.print("5\n"); break;
+  
+  case 6: Serial.print("6\n"); break;
+  delay(500);
+  default:  break;
+  }
+  delay(500);
+}
