252fc2aceb9778c66bc7957f85675575aabc177c
Rajmond1999 <72790807+Rajmond1999@users.noreply.github.com>
2020-12-30 11:13:53 +0200

    Update Arduino_code.ino

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -1,10 +1,18 @@
+const int ledPin = 13; 
+
 void setup() {
-  // put your setup code here, to run once:
-  
-  //aaa
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  char code=0;
+  //  reading code
+  if (Serial.available() > 0) {
+    code = Serial.read();
+    }
+    if(code=='1')
+    {
+      digitalWrite(ledPin, !digitalRead(ledPin));
+      }
 }
